475fb13539ac95024016dfde9cbd2ddbce4e82e9
guilhermeribeirro <gui-souza2012@hotmail.com>
2024-03-10 18:46:54 -0300

    Questao1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+interface OlaProps{
+
+
+    nomeProps:string
+
 }
 
+const Ola : React.FC<OlaProps> =({nomeProps}) => {
+    const [contadorLikes, setContadorLikes]= useState<number>(0);
+
+    const adicionarContadorLikes =() => setContadorLikes(contadorLikes + 1);
+    const diminuirContadorLikes = () => setContadorLikes(contadorLikes -1);
+    return (
+        <View style = {styles.container}>
+
+            <Text style = {styles.greeting}>
+
+
+                Olá {nomeProps}, voce possui {contadorLikes} Likes.
+            </Text>
+            <View style = {styles.buttonContainer}>
+
+                    <TouchableOpacity
+                    onPress={adicionarContadorLikes}
+                    style={[styles.button, {backgroundColor: 'blue'}]}
+                >
+                <Text style = {styles.buttonText}>Like</Text>
+
+                </TouchableOpacity>
+                <TouchableOpacity
+
+                onPress={diminuirContadorLikes}
+                style={[styles.button, {backgroundColor: 'red'}]}
+>
+
+                    <Text style={styles.buttonText}> Deslike</Text>
+                    </TouchableOpacity>
+                </View>
+                </View>
+                );
+
+
+
+    
+   }
+
 const styles = StyleSheet.create({
-  container: {
+
+container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+
+},
+
+greeting: {
+
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 16,
+},
+buttonContainer: {
+
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '80%',
+
+
+},
+button: {
+    width: '100%',
+    marginVertical: 5,
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+   
+},
+
+buttonText: {
+
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+},
 });
+
+export default Ola;
+
+
+
+
+
+
+
+
+
